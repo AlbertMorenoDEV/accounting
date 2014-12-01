cb98bd5b0ea119d00e6467f124e0ad19497496fd
albert.moreno <albert.moreno@nodoambar.com>
2014-12-02 00:56:11 +0100

    List account histories

--- src/clients/web/templates/list-account-histories.tpl.php
+++ src/clients/web/templates/list-account-histories.tpl.php
@@ -5,27 +5,23 @@
 </ol>
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h2 class="panel-title">List of Accounts</h2>
+		<h2 class="panel-title">List of Account histories</h2>
 	</div>
 	<div class="panel-body">
 		<table class="table">
 			<tr>
-				<th>Name</th>
-				<th>Balance</th>
-				<th> </th>
+				<th>Date</th>
+				<th>Concept</th>
+				<th>Amount</th>
 				<th> </th>
 			</tr>
-			<?php foreach ($data as $account) { ?>
+			<?php foreach ($data as $accountHistory) { ?>
 				<tr>
-					<td><a href="ViewAccount?id=<?= $account->getId() ?>"><?= $account->getName() ?></a></td>
-					<td><?= $account->getTotal() ?></td>
+					<td><?= $accountHistory->getDate()->format('Y-m-d H:i:s') ?></td>
+					<td><a href="ViewAccountHistory?id=<?= $accountHistory->getId() ?>"><?= $accountHistory->getConcept() ?></a></td>
+					<td><?= $accountHistory->getAmount() ?></td>
 					<td>
-						<a href="ListAccountHistories?id_account=<?= $account->getId() ?>">
-							<span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
-						</a>
-					</td>
-					<td>
-						<a href="ConfigureAccount?id=<?= $account->getId() ?>">
+						<a href="ConfigureAccountHistory?id=<?= $accountHistory->getId() ?>">
 							<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
 						</a>
 					</td>

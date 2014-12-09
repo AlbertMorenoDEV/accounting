537d40480563240b2f508a31f1992a756b6bcb7b
albert.moreno <albert.moreno@nodoambar.com>
2014-12-09 23:47:00 +0100

    code refactoring

--- src/clients/cli/commands/ModifyAccountNameCommand.php
+++ src/clients/cli/commands/ModifyAccountNameCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 use accounting\aplication\ModifyAccountName;
 
-class ChangeAccountNameCommand extends Command
+class ModifyAccountNameCommand extends Command
 {
 	private $repo;
 	private $idsGenerator;

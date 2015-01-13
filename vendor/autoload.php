ca1a396a45630933595684f9949b3fb8228a349e
albert.moreno <albert.moreno@nodoambar.com>
2015-01-13 23:35:30 +0100

    jQuery & jQuery UI install

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf3c74de743fc94e1199550ab0acd7b3c::getLoader();
+return ComposerAutoloaderInitb49adaebb5c04b0f3844eded1663b0c1::getLoader();

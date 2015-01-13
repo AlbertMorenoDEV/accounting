32da7a58ecccfa71d12df3bb5b037af97b530d6e
albert.moreno <albert.moreno@nodoambar.com>
2015-01-14 00:04:15 +0100

    alerts

--- src/clients/web/templates/create-account.tpl.php
+++ src/clients/web/templates/create-account.tpl.php
@@ -8,12 +8,9 @@
 		<h2 class="panel-title">Account</h2>
 	</div>
 	<div class="panel-body">
-		<ul class="list-group">
-			<li class="list-group-item">
-				<h3 class="list-group-item-heading">Id:</h3>
-				<p class="list-group-item-text"><?= $data->getId() ?></p>
-			</li>
-		</ul>
+		<div class="alert alert-success" role="alert">
+			<b>New item, Id:</b> <?= $data->getId() ?>
+		</div>
 	</div>
 	<div class="panel-footer"><a href="javascript:history.back()">Back</a></div>
 </div>
